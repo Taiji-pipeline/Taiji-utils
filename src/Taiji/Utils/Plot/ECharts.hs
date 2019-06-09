14115137475e7eccc6744b3b9054299b0049fb25
Kai Zhang <kai@kzhang.org>
2019-06-09 10:04:22 -0700

    modify scatter2D

--- src/Taiji/Utils/Plot/ECharts.hs
+++ src/Taiji/Utils/Plot/ECharts.hs
@@ -31,6 +31,7 @@ data VisualMap = Continuous [Double]
                | Categorical [String]
 
 type Point3D = (Double, Double, Double)
+type Point2D = (Double, Double)
 
 scatter3D :: [(String, [Point3D])]
           -> VisualMap
@@ -129,9 +130,111 @@ scatter3D dat viz = EChart $ \eid -> displayT $ renderOneLine $ renderJs $
             inRange: { symbol: { '': 'squre' } }
         } |]
 
-
-scatter :: [(String, [[Double]])]
+scatter :: [(String, [Point2D])]
+        -> VisualMap
         -> EChart
+scatter dat viz = EChart $ \eid -> displayT $ renderOneLine $ renderJs $
+    [jmacro|
+        var myChart = echarts.init(document.getElementById(`(eid)`));
+        var !dataset = `dataPoints`;
+        myChart.setOption(`option`);
+    |]
+  where
+    dataPoints =
+        let f (nm, (x, y)) v = (nm, [toJSON x, toJSON y, toJSON v])
+        in toJSON $ HM.fromList $ map (first head . unzip) $ groupBy ((==) `on` fst) $ case viz of
+            Continuous vs -> zipWith f points vs
+            Categorical vs ->zipWith f points vs
+      where
+        points = concatMap (\(x,y) -> zip (repeat x) y) dat
+    option = [jmacroE| {
+        grid: [ {
+            width : "40%",
+            show:true
+        }, {
+            width : "40%",
+            left: "50%",
+            show:true
+        } ],
+        xAxis: [ {
+            gridIndex: 0,
+            axisTick: {show:false},
+            axisLabel: {show:false},
+            splitLine: {show:false},
+            axisLine: {onZero: false},
+            name: "dim1"
+        }, {
+            gridIndex: 1,
+            axisTick: {show:false},
+            axisLabel: {show:false},
+            splitLine: {show:false},
+            axisLine: {onZero: false},
+            name: "dim1"
+        } ],
+        yAxis: [ {
+            gridIndex: 0,
+            axisTick: {show:false},
+            axisLabel: {show:false},
+            splitLine: {show:false},
+            axisLine: {onZero: false},
+            name: "dim2"
+        }, {
+            gridIndex: 1,
+            axisTick: {show:false},
+            axisLabel: {show:false},
+            splitLine: {show:false},
+            axisLine: {onZero: false},
+            name: "dim2"
+        } ],
+        series: `map (mkSeries 1) dat ++ map (mkSeries 0) dat`,
+        legend: { data: `map fst dat` },
+        visualMap: `visualMap`,
+        color: [ 
+            "#ff7f50", "#87cefa", "#da70d6", "#32cd32", "#6495ed", 
+            "#ff69b4", "#ba55d3", "#cd5c5c", "#ffa500", "#40e0d0", 
+            "#1e90ff", "#ff6347", "#7b68ee", "#00fa9a", "#ffd700", 
+            "#6b8e23", "#ff00ff", "#3cb371", "#b8860b", "#30e0e0" 
+        ],
+        toolbox: {
+            show: true,
+            feature: {
+                saveAsImage: {
+                    pixelRatio: 3,
+                    excludeComponents: ["toolbox"]
+                }
+            }
+        }
+    } |]
+    mkSeries i (label, _) = [jmacroE| {
+        xAxisIndex: `i::Int`,
+        yAxisIndex: `i::Int`,
+        type: 'scatter',
+        symbolSize: 3,
+        name: `label`,
+        data: dataset[`label`]
+    } |]
+    visualMap = case viz of
+        Continuous vs -> [jmacroE| {
+            seriesIndex: `[0 .. length dat - 1]`,
+            precision: 2,
+            calculable: true,
+            min: `minimum vs`,
+            max: `maximum vs`,
+            right: 10,
+            inRange: {
+                color: ["#50a3ba", "#eac736", "#d94e5d"]
+            }
+        } |]
+        Categorical c -> [jmacroE| {
+            type: "piecewise",
+            seriesIndex: `[0 .. length dat - 1]`,
+            right: 10,
+            categories: `nubSort c`,
+            inRange: { symbol: { '': 'squre' } }
+        } |]
+
+
+        {-
 scatter series = EChart $ \eid -> displayT $ renderOneLine $
     renderJs $ [jmacro|
         var myChart = echarts.init(document.getElementById(`(eid)`));
@@ -151,6 +254,7 @@ scatter series = EChart $ \eid -> displayT $ renderOneLine $
         type: 'scatter',
         symbolSize: 3
     } |]
+    -}
 
 {-
 stackBar :: T.Text   -- ^ title
