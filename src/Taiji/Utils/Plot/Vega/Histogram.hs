827d3fcc2e8a832f8cbae31fb1ab0a54b80b5556
Kai Zhang <kai@kzhang.org>
2019-07-19 10:44:07 -0700

    minor

--- src/Taiji/Utils/Plot/Vega/Histogram.hs
+++ src/Taiji/Utils/Plot/Vega/Histogram.hs
@@ -7,15 +7,15 @@ import Language.Javascript.JMacro
 
 import Taiji.Utils.Plot.Vega.Types
 
-hist :: [Double] -> Vega
-hist input = option [jmacroE| {
+hist :: [Double] -> Int -> Vega
+hist input nbins = option [jmacroE| {
     $schema: "https://vega.github.io/schema/vega-lite/v3.json",
     data: {values: `dat`},
     layer: [ {
         mark: "bar",
         encoding: {
             x: {
-                bin: true,
+                bin: {maxbins: `nbins`},
                 field: "x",
                 type: "quantitative"
             },
