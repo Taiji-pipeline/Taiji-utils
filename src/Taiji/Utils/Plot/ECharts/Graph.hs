16ee982593064712068a3093be832c2e5f2f55c5
Kai Zhang <kai@kzhang.org>
2019-10-13 13:09:27 -0700

    minor

--- src/Taiji/Utils/Plot/ECharts/Graph.hs
+++ src/Taiji/Utils/Plot/ECharts/Graph.hs
@@ -7,28 +7,22 @@ import Language.Javascript.JMacro
 
 import Taiji.Utils.Plot.ECharts.Types
 
-graph :: [(String, Double)] -> [(String, String)] -> EChart
-graph nodes edges = mkEChart [jmacroE| {
+graph :: (String -> JExpr) -> [String] -> [(String, String)] -> EChart
+graph f nodes edges = mkEChart [jmacroE| {
     series: {
         type: "graph",
         layout: "force",
-        data: `nodes'`,
+        data: `map f nodes`,
         links: `links`,
-        focusNodeAdjacency: true
+        focusNodeAdjacency: true,
+        draggable: true,
+        roam: true,
+        edgeSymbol: ["none", "arrow"],
+        edgeSymbolSize: 3,
+        force: {
+            repulsion: 70
+        }
     }
     }|]
   where
-    nodes' = flip map (zip names $ linearMap (10, 50) vals) $ \(x, v) -> [jmacroE| {
-        name: `x`,
-        symbolSize: `v`
-    }|]
     links = flip map edges $ \(fr, to) -> [jmacroE| {source: `fr`, target: `to`} |]
-    (names, vals) = unzip nodes
-
-linearMap :: (Double, Double) -> [Double] -> [Double]
-linearMap (lo, hi) xs = map f xs
-    where
-    f x = lo + (x - min') / (max' - min') * (hi - lo)
-    min' = minimum xs
-    max' = maximum xs
-{-# INLINE linearMap #-}
