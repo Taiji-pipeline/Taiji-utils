0a163f9d76b79c1b70fa16e771cd2c1e3d51e5d4
Kai Zhang <kai@kzhang.org>
2021-05-01 07:33:54 -0700

    minor

--- src/Taiji/Types.hs
+++ src/Taiji/Types.hs
@@ -74,7 +74,6 @@ data SCATACSeqOptions = SCATACSeqOptions
     , _scatac_cell_barcode_length :: Maybe Natural
     , _scatac_te_cutoff :: Double
     , _scatac_minimal_fragment :: Natural
-    , _scatac_remove_doublets :: Bool
     , _scatac_doublet_score_cutoff :: Double
     , _scatac_window_size :: Natural
     } deriving (Generic)
@@ -100,7 +99,6 @@ instance FromJSON SCATACSeqOptions where
         <*> v .:? "cell_barcode_length"
         <*> v .:? "tss_enrichment_cutoff" .!= 5
         <*> v .:? "fragment_cutoff" .!= 1000
-        <*> v .:? "remove_doublets" .!= True
         <*> v .:? "doublet_score_cutoff" .!= 0.5
         <*> v .:? "window_size" .!= 5000
       where
